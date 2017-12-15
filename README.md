bbcd07d5e26fb5d1d5bf4535b5341f3ae4caff48
catlover91 <7215827+CatLover91@users.noreply.github.com>
2017-12-15 21:24:03 +0000

    more literal install

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # moe
 the swiss army tool of rice
+
+experimental
+```
+curl -o- https://raw.githubusercontent.com/CatLover91/moe/master/install.sh | bash
+```
--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
-command curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
+
+nvm install node
+npm install -g https://github.com/CatLover91/moe.git
